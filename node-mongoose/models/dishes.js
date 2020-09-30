023a440d3520b50d4a335ad04402511e7a665e36
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-30 17:16:59 -0300

    Mongoose Part 1

---
+++ node-mongoose/models/dishes.js
@@ -0,0 +1,18 @@
+const {Schema,model} = require('mongoose');
+
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+
+module.exports = model('Dish', dishSchema);
