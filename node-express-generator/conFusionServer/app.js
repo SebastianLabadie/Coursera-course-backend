5732e8dc38791f763e409539fc39dd9609867e53
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-30 18:34:15 -0300

    Basic Authentication

--- node-express-generator/conFusionServer/app.js
+++ node-express-generator/conFusionServer/app.js
@@ -58,4 +58,30 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
+
 module.exports = app;
